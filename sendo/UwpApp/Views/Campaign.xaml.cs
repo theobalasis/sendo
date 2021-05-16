4e3f41481d02112fa02dce38683bf85c85f15c72
Theo Balasis <theodorosbalasis@gmail.com>
2021-05-16 13:14:08 +0300

    Complete restructuring

---
+++ sendo/UwpApp/Views/Campaign.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Sendo.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Campaign : Page
+    {
+        public Campaign()
+        {
+            this.InitializeComponent();
+        }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void filebuttonclick(object sender, RoutedEventArgs e)
+        {
+
+            FileOpenPicker openPicker = new FileOpenPicker();
+            openPicker.ViewMode = PickerViewMode.Thumbnail;
+            openPicker.SuggestedStartLocation = PickerLocationId.DocumentsLibrary;
+            openPicker.FileTypeFilter.Add(".rtf");
+            StorageFile file = await openPicker.PickSingleFileAsync();
+            if (file != null)
+            {
+                filebutton.Content = file.Name;
+            }
+            else
+            {
+                filebutton.Content = "Choose File";
+            }
+        }
+
+        private void StartB_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
